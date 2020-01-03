1273008cc939721356047b41173b74458ea10128
akane2002 <akane2002@gmail.com>
2020-01-04 00:55:06 +1300

    Create new project and setup unity test environment

---
+++ Assets/Scripts/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worker : MonoBehaviour
+{
+	public int mCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+		mCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		mCounter += 1;
+	}
+}
---
+++ Assets/Tests/FirstTestScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FirstTestScript
+    {
+		GameObject goTest;
+
+		[SetUp]
+		public void Setup()
+		{
+			goTest = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/PrefabWorker"));
+		}
+
+		[TearDown]
+		public void Cleanup()
+		{
+			GameObject.Destroy(goTest);
+		}
+
+		// A Test behaves as an ordinary method
+		[Test]
+        public void ICanInstantiatePrefabGameObjectInSimplePasses()
+        {
+			// Use the Assert class to test conditions
+			Assert.IsTrue(goTest != null);
+			Assert.IsTrue(goTest.GetComponent<Worker>() != null);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ICanInstantiatePrefabGameObjectInEnumeratorPasses()
+        {
+			// Use the Assert class to test conditions.
+			// Use yield to skip a frame.
+			Assert.IsTrue(goTest != null);
+			Assert.IsTrue(goTest.GetComponent<Worker>() != null);
+			yield return null;
+        }
+
+		[Test]
+		public void ICanVerityTheStartStateInSimplePasses()
+		{
+			// Use the Assert class to test conditions
+			Assert.IsTrue(goTest != null);
+			Assert.IsTrue(goTest.GetComponent<Worker>() != null);
+			Assert.AreEqual(0, goTest.GetComponent<Worker>().mCounter);
+		}
+
+		[UnityTest]
+		public IEnumerator ICanVerityTheStartStateInEnumeratorPasses()
+		{
+			// Use the Assert class to test conditions.
+			// Use yield to skip a frame.
+			Assert.IsTrue(goTest != null);
+			Assert.IsTrue(goTest.GetComponent<Worker>() != null);
+			int counter_before = goTest.GetComponent<Worker>().mCounter;
+			yield return null;
+			Assert.AreEqual(counter_before + 1, goTest.GetComponent<Worker>().mCounter);
+			yield return null;
+		}
+	}
+}
